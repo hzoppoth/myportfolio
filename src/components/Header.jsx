d983f029b66fcb1d73023a47e8b74b5c79a5216e
hzoppoth <huntysvt@gmail.com>
2024-04-25 19:10:46 -0400

    Added favicon, fixed typo, added highlighter to nav bar items.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import '../Global.css'
-import '../Header.css'
+import { NavLink } from 'react-router-dom';
+import '../Global.css';
+import '../Header.css';
 
 function Header() {
     return (
         <header>
             <nav id="global-div">
                 <ul id="nav-items">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/projects">Projects</Link></li>
-                    <li><Link to="/aboutme">About Me</Link></li>
+                    <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+                    <li><NavLink to="/projects" activeClassName="active">Projects</NavLink></li>
+                    <li><NavLink to="/aboutme" activeClassName="active">About Me</NavLink></li>
                 </ul>
             </nav>
         </header>
-    )
+    );
 }
 
 export default Header;
